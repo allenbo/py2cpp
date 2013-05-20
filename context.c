0565a6cf5756a9792c73c25479858a31258a0f96
abo <linjianfengqrh@gmail.com>
2013-05-20 01:13:41 -0400

    add context for parser

---
+++ context.c
@@ -0,0 +1,19 @@
+#include "context.h"
+
+static context_ty con = NULL;
+
+
+static context_ty
+init_con() {
+    context_ty con = (context_ty) malloc (sizeof( context));
+    memset(con, 0, sizeof(context));
+    return con;
+}
+
+context_ty
+get_context() {
+    if(NULL == con) {
+        con = init_con();
+    }
+    return con;
+}
---
+++ context.h
@@ -0,0 +1,12 @@
+#ifndef _CONTEXT_H_
+#define _CONTEXT_H_
+
+
+typedef context* context_ty;
+
+typedef struct {
+    comp_table
+}context;
+
+
+#endif
